0ce39dfd80cb17f7aa93166dc00b6a1f48672f60
Asadullah <140200762+Ich-Asadullah@users.noreply.github.com>
2024-05-23 02:56:20 +0500

    Added an Extended version of a built in class.

---
+++ Services/MyObjectOutputStream.java
@@ -0,0 +1,16 @@
+package Models;
+
+import java.io.*;
+
+public class MyObjectOutputStream extends ObjectOutputStream {
+    public MyObjectOutputStream() throws IOException {
+        super();
+    }
+
+    public MyObjectOutputStream(OutputStream o) throws IOException {
+        super(o);
+    }
+
+    public void writeStreamHeader() {
+    }
+}
